a18f0c46e88b80e7017ae9175d555c4d972133dc
ibirothe <ibirothe@gmail.com>
2025-09-30 01:45:01 +0200

    new project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -5,6 +5,7 @@ import Particle from "../Particle";
 import kings_guard from "../../Assets/Projects/kings_guard.png";
 import park from "../../Assets/Projects/park.png";
 import jay from "../../Assets/Projects/jay2door.png";
+import lorem from "../../Assets/Projects/react_web.png";
 
 function Projects() {
   return (
@@ -25,6 +26,17 @@ function Projects() {
               title="King´s Guard"
               description="Rundenbasiertes Strategie-Spiel, entwickelt mit der Godot Engine und auf Steam mit über 1.000 Wishlists veröffentlicht. Integriert Steamworks-API-Funktionen wie Errungenschaften, Bestenlisten und Cloud-Speicherung. Kuratiert von Games Germany, Tiny Teams, ANZ Indie Showcase, Debut Festival und Steam Board Games Festival."
               demoLink="https://store.steampowered.com/app/3720900/Kings_Guard/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={lorem}
+              isBlog={false}
+              title="React Webpage Template"
+              description="React single-page app demonstrating dynamic routing, component-based state management and prop-driven image reveal animations. Integrates @cloudinary/react for optimized image delivery, react-waypoint for scroll-triggered effects React single-page app mit dynamic routing, component-based state management und prop-driven image reveal animations."
+              ghLink="https://github.com/ibirothe/react_website_template"
+              demoLink="https://webloremipsum.vercel.app"        
             />
           </Col>
 
